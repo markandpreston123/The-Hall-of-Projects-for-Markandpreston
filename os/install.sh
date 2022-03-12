4ba85d2cc68258b4510caea2102fa4b6cfe1f713
Markandpreston <100131830+markandpreston123@users.noreply.github.com>
2022-03-12 15:25:46 +0200

    Update install.sh

--- os/install.sh
+++ os/install.sh
@@ -1,1 +1,4 @@
-
+echo Installing...
+wget --content-disposition \
+"https://raw.githubusercontent.com/markandpreston123/The-Hall-of-Projects-for-Markandpreston/main/os/main.py"
+echo Installation finished.
